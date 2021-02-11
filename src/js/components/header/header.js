b73d5ac156de37a9125903a8615f0432f4a083f0
Nathan Griffiths <ngriffiths@postmedia.com>
2021-02-10 21:48:42 -0800

    added subhead

--- src/js/components/header/header.js
+++ src/js/components/header/header.js
@@ -6,6 +6,7 @@ const init = async (data, el) => {
 	const head = document.querySelector(el);
 	head.innerHTML = `
 			<h2>The shape of COVID-19 deaths in Canada</h2>
+			<p>Cumulative COVID-19 related deaths by province.</p>
 		`;
 }
 
